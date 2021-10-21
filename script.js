242dabdbf32ac5366fd99e7fa0a5f9dfa5fba9bb
CYRENE <xc2234@nyu.edu>
2021-10-21 10:38:47 -0400

    ok

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById("one").addEventListener("mouseover", function () {
+    document.getElementsByClassName("topcircle")[0].style.backgroundColor = "#2b54ac3b";
+    document.getElementsByClassName("leftcircle")[0].style.backgroundColor = "#ff8b2d81";
+    document.getElementsByClassName("rightcircle")[0].style.backgroundColor = "#ffcc266c";
+});
+document.getElementById("one").addEventListener("mouseout", function () {
+    document.getElementsByClassName("topcircle")[0].style.backgroundColor = "#cde5ff63";
+    document.getElementsByClassName("leftcircle")[0].style.backgroundColor = "#fad1d169";
+    document.getElementsByClassName("rightcircle")[0].style.backgroundColor = "#ffeeb662";
+});
+
+document.getElementById("two").addEventListener("mouseover", function () {
+    document.getElementsByClassName("topcircle")[0].style.backgroundColor = "#ffa38c79";
+    document.getElementsByClassName("leftcircle")[0].style.backgroundColor = "#91d8cb70";
+    document.getElementsByClassName("rightcircle")[0].style.backgroundColor = "#ffd4bb70";
+});
+document.getElementById("two").addEventListener("mouseout", function () {
+    document.getElementsByClassName("topcircle")[0].style.backgroundColor = "#cde5ff63";
+    document.getElementsByClassName("leftcircle")[0].style.backgroundColor = "#fad1d169";
+    document.getElementsByClassName("rightcircle")[0].style.backgroundColor = "#ffeeb662";
+});
+
+document.getElementById("three").addEventListener("mouseover", function () {
+    document.getElementsByClassName("topcircle")[0].style.backgroundColor = "#a4ecc8c9";
+    document.getElementsByClassName("leftcircle")[0].style.backgroundColor = "#29a58270";
+    document.getElementsByClassName("rightcircle")[0].style.backgroundColor = "#4fb2cab7";
+});
+document.getElementById("three").addEventListener("mouseout", function () {
+    document.getElementsByClassName("topcircle")[0].style.backgroundColor = "#cde5ff63";
+    document.getElementsByClassName("leftcircle")[0].style.backgroundColor = "#fad1d169";
+    document.getElementsByClassName("rightcircle")[0].style.backgroundColor = "#ffeeb662";
+});
